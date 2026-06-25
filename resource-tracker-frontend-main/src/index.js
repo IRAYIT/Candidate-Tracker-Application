9587ab368d121e5da25f9d5c18ec482426426ec8
Subrahmanyam246 <subrahmanyam.tadi@ikonicit.com>
2026-06-25 14:59:37 +0530

    fix css import

--- resource-tracker-frontend-main/src/index.js
+++ resource-tracker-frontend-main/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store'; // Make sure your store file path is correct
-import './output.css';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
